ad13cc156df92be5b5a710d366aa4ac0d06f0322
skytreader <chadestioco@gmail.com>
2013-06-08 20:36:13 +0800

    Initial commit with some misplaced scrappy code.

---
+++ crawler.py
@@ -0,0 +1,28 @@
+from scrapy.item import Item, Field
+from scrapy.contrib.spiders import CrawlSpider
+from scrapy.contrib.linkextractors.sgml import SgmlLinkExtractor
+from scrapy.selector import HtmlXPathSelector
+
+class GutenbergItem(Item):
+    title = Field()
+    plain_text_url = Field()
+
+class GutenbergSpider(CrawlSpider):
+    
+    name = "gutenberg.org"
+    allowed_domains = ["gutenberg.org"]
+    # Hard code for King Arthur for now...
+    start_urls = ["http://www.gutenberg.org/ebooks/12753"]
+    rules = [Rule(SgmlLinkExtractor(allow=["/ebooks/\d+"])), "parse_book_record")]
+
+    def parse_book_record(self, response):
+        self.log("Parsing %s", response.url)
+
+        selector = HtmlXPathSelector(response)
+
+        book = GutenbergItem()
+        # But what if there are more than one h1s in the document?
+        # Weird itemprop attribute in the gutenberg code.
+        book["title"] = selector.select("//h1/text()").extract()
+        spam = selector.select("//a[@class='link']").extract()
+        print spam
