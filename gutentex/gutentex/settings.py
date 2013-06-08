dc33ed0ea4018e322d93a37bd5cffc0fdea4de0a
skytreader <chadestioco@gmail.com>
2013-06-08 20:50:08 +0800

    Scrapy project

---
+++ gutentex/gutentex/settings.py
@@ -0,0 +1,15 @@
+# Scrapy settings for gutentex project
+#
+# For simplicity, this file contains only the most important settings by
+# default. All the other settings are documented here:
+#
+#     http://doc.scrapy.org/topics/settings.html
+#
+
+BOT_NAME = 'gutentex'
+
+SPIDER_MODULES = ['gutentex.spiders']
+NEWSPIDER_MODULE = 'gutentex.spiders'
+
+# Crawl responsibly by identifying yourself (and your website) on the user-agent
+#USER_AGENT = 'gutentex (+http://www.yourdomain.com)'
